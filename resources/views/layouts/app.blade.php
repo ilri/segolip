7e8f4e3c05e976416f2f46e52ed0db6a75a538d6
maldeve <malandro624@gmail.com>
2022-01-06 11:28:16 +0300

    changed css style path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 
     <!-- Livewire -->
     @livewireStyles
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,9 +8,9 @@
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> -->
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <link href="css/app.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/dashboard.css">
 </head>
 
 <body>
