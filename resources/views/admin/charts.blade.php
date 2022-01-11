4fd97b573e0881246daefeeb6f997500ec8440ec
maldeve <malandro624@gmail.com>
2022-01-11 11:20:25 +0300

    push items to services array

--- resources/views/admin/charts.blade.php
+++ resources/views/admin/charts.blade.php
@@ -7,7 +7,8 @@
         $dataPoints = array();
         $dataPoints = array_push($dataPoints, array_values(explode(",", $service['quantity_sold'])));
 
-        $labels = array(array_values(explode(",", $service['name'])));
+        $labels = array();
+        $labels = array_push($labels, array_values(explode(",", $service['name'])));
     }
 
     // orders
